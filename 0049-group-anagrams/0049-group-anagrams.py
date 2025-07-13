e3c9282f0d2db4653bb0d2c8f195cc9a33a721a4
Elias Firisa <125973507+EliasSf73@users.noreply.github.com>
2025-07-13 19:22:46 +0900

    Time: 11 ms (82.85%), Space: 20.6 MB (76.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         maps=dict()
-        output=[]
         for a in strs:
-            
-            kee=''.join(sorted(list(a)))
+            kee= ''.join(sorted(list(a)))
             if kee in maps:
                 maps[kee].append(a)
             else:
                 maps[kee]=[a]
         return [k for k in maps.values()]
 
+       
 
-        
-        
+
