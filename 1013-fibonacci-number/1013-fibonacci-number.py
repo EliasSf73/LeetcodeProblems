222f15071a2fa77a38e471f509b8c4e2081bf3ef
Elias Firisa <125973507+EliasSf73@users.noreply.github.com>
2025-10-07 05:12:46 +0900

    Time: 1186 ms (5.19%), Space: 12.5 MB (10.31%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,7 +1,22 @@
-class Solution:
-    def fib(self, n: int) -> int:
+class Solution(object):
+     # cache to store results
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        memo_cache=dict()
+        # if result already in cache, return it
+        if n in memo_cache:
+            return memo_cache[n]
+        # else, typically recursion
         if n<=1:
             return n
         else:
-            return self.fib(n-1) + self.fib(n-2)
-        
+            result= self.fib(n-1) + self.fib(n-2)
+        # save result to memom_cache
+        memo_cache[n]= result
+
+        return result
+
+    
